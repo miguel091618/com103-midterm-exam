b93f2339a45e9ccf91b0b0c63f51391608d797fb
magno.miguel <miguelmagno042007@gmail.com>
2026-04-25 21:55:48 +0800

    Added Initial Variables

--- midterm_solution.py
+++ midterm_solution.py
@@ -1,1 +1,15 @@
+project_title = input("Project : ")
+group_name = input("Group : ")
 
+task_names = [
+    "Program Logic / Coding",
+    "UI / Output Formatting",
+    "Testing / Debugging",
+    "Documentation / README",
+    "Presentation Slides"
+]
+
+task_hours = [6, 3, 2, 2, 2]
+print("\n=====================================")
+print("    COM 103 PROJECT -- TASK TYPES")
+print("=====================================")
